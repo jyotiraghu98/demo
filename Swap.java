979d5479a1d2d944a5361861a5809bb400d64073
Jyoti Raghu <jyotiraghu@gmail.com>
2023-01-25 15:41:48 +0530

    First commit

---
+++ Swap.java
@@ -0,0 +1,12 @@
+
+
+public class Swap{
+public static void main(String arg[]){
+int a=10, b=20, swap=0;
+System.out.println("Before Swapping" +a+" "+b);
+a = swap;
+swap = b;
+b = a;
+System.out.println("after Swapping" +a+" "+b);
+}
+}
